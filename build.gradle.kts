b243f2f78b011a660a96d309c5838ab7c29f721d
Matouš Kučera <mk@kcra.me>
2026-09-19 12:08:32 +0200

    chore: bump TeaVM

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.teavm) // order matters?
 }
 
-val thisVersion = "0.1.6"
+val thisVersion = "0.1.7"
 
 group = "run.slicer"
 version = "$thisVersion-${libs.versions.cfr.get()}"
